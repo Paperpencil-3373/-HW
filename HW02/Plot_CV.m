c9388556d2708b5ac18028f6446ff388da8e7b65
YYLab_PC_05 <y989830@gmail.com>
2023-11-06 22:42:18 +0800

    HW02 complete: 2023_1106

--- HW02/Plot_CV.m
+++ HW02/Plot_CV.m
@@ -1,16 +1,49 @@
 
-% export save path
-path = "D:\Matlab_Savefile\程控HW\HW02\PictureSave"
+%% export save path
 
+today_date = "2023_1106"
 
+path = strcat( ...
+    "D:\Matlab_Savefile\程控HW\HW02\PictureSave\", ...
+    today_date)
+mkdir(path)
+
+%% Plot: Pulse change
 % import simout data
-pulse_change = out.simout_CV.Data
+pulse_change = out.simout_CV_pulsechange.Data
 
 % plot the picture
-f1 = figure('name', 'Figure_CV')
+f1 = figure('name', 'Figure_PulseChange')
 
 plot(pulse_change)
 title("CV pulse change")
 
-% saveas(f1, ...
-%     strcat(path, '\', 'pulse_change_CV.png'))
+saveas(f1, ...
+    strcat(path, '\', 'pulse_change_CV.png'))
+
+%% Plot: Feedforward control
+% import simout data
+simout_CV_fforward = out.simout_CV_fforward.Data
+
+% plot the picture
+f2 = figure('name', 'Figure_')
+
+plot(simout_CV_fforward)
+title("Feedforward Control")
+
+saveas(f2, ...
+    strcat(path, '\', 'simout_CV_fforward.png'))
+
+
+%% Plot: Feedback control
+% import simout data
+simout_CV_fback= out.simout_CV_fback.Data
+
+% plot the picture
+f3 = figure('name', 'Figure_')
+
+plot(simout_CV_fback)
+title("Feedback Control")
+
+saveas(f3, ...
+    strcat(path, '\', 'simout_CV_fback.png'))
